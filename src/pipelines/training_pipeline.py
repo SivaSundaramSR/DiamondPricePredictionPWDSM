de1320dbe582aa03aa50cb487a05baf9ebeecb9a
Siva Sundaram S R <srss.siva@hotmail.com>
2023-08-24 22:23:14 +0530

    project upload

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,9 +5,16 @@ from src.exception import CustomException
 import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 if __name__ == "__main__":
-    obj=DataIngestion()
-    train_data_path, test_data_path = obj.initiate_data_ingestion()
-    
+    data_ingestion_obj = DataIngestion()
+    train_data_path, test_data_path = data_ingestion_obj.initiate_data_ingestion()
     print(train_data_path, test_data_path)
+    
+    data_transformation_obj = DataTransformation()
+    train_arr, test_arr, preprocessor_pkl_file_path = data_transformation_obj.initiate_data_transformation(train_data_path, test_data_path)
+    
+    model_trainer_obj = ModelTrainer()
+    model_trainer_obj.initiate_model_trainer(train_arr, test_arr)
